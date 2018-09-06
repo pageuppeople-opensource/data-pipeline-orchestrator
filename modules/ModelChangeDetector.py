064ab86b7d6d6ccc91dc563495768133e0db16b2
Chintan Raval <chintanr@pageuppeople.com>
2018-09-06 11:25:53 +1000

    lint it

--- modules/ModelChangeDetector.py
+++ modules/ModelChangeDetector.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import uuid
 
+
 class ModelChangeDetector(object):
 
     _appName = 'model-change-detector'  # TODO: where to save-this and read-this-from?
@@ -12,11 +13,13 @@ class ModelChangeDetector(object):
 
     def __init__(self, logger=None):
         self.logger = logger or logging.getLogger(__name__)
+
         self._logLevelStrings = [logging.getLevelName(logging.CRITICAL),
-                                logging.getLevelName(logging.ERROR),
-                                logging.getLevelName(logging.WARNING),
-                                logging.getLevelName(logging.INFO),
-                                logging.getLevelName(logging.DEBUG)]
+                                 logging.getLevelName(logging.ERROR),
+                                 logging.getLevelName(logging.WARNING),
+                                 logging.getLevelName(logging.INFO),
+                                 logging.getLevelName(logging.DEBUG)]
+
         self._defaultLogLevelString = logging.getLevelName(logging.INFO)
 
     def main(self):
@@ -24,23 +27,28 @@ class ModelChangeDetector(object):
         self.configure_logging(args.log_level)
 
         # TODO add logic to
-        # - figure out how to separate args.execution_mode code/logic.. a class per execution-mode? IExecutionMode.Execute(...)? DI?
-        # - connect to db using args.db_connection_string and add a record to DataPipelineExecution table with str(uuid.uuid4())
+        # - figure out how to separate args.execution_mode code/logic..
+        # a class per execution-mode? IExecutionMode.Execute(...)? DI?
+        # - connect to db using args.db_connection_string and add a record to DataPipelineExecution table
+        # with str(uuid.uuid4())
         execution_result = str(uuid.uuid4())
-        
+
         if args.verbose:
             print(args)
             print(f'args.log_level = {args.log_level} = {logging.getLevelName(args.log_level)}')
             print(self.logger)
             print(f'execution_result = {execution_result}')
+
         return execution_result
 
     def configure_logging(self, log_level):
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         console_stream_handler = logging.StreamHandler()
         console_stream_handler.setFormatter(formatter)
+
         self.logger.addHandler(console_stream_handler)
         self.logger.setLevel(log_level)
+
         return
 
     def get_arguments(self):
@@ -53,11 +61,13 @@ class ModelChangeDetector(object):
                             action='store',
                             # metavar='execution-mode',
                             choices=ModelChangeDetector._executionModes,
-                            help='execution mode; choose from {0}'.format(", ".join(ModelChangeDetector._executionModes)))
+                            help='execution mode; choose from {0}'.format(
+                                ", ".join(ModelChangeDetector._executionModes)))
 
         parser.add_argument('db_connection_string',
                             metavar='db-connection-string',
-                            help='database connection string, e.g. postgresql+psycopg2://postgres:xxxx@localhost/dest_dw')
+                            help='database connection string, e.g. '
+                                 'postgresql+psycopg2://postgres:xxxx@localhost/dest_dw')
 
         args = parser.parse_args()
         return args
@@ -98,7 +108,7 @@ class ModelChangeDetector(object):
             raise argparse.ArgumentTypeError(message)
 
         log_level_int = getattr(logging, log_level_string, logging.INFO)
-   
+
         # check the logging log_level_choices have not changed from our expected values
         assert isinstance(log_level_int, int)
 
