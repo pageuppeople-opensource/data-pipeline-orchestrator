f7343be88ae0ab30dd0a06b13b3be93297fad975
Walter Ngo <ngo.walter@gmail.com>
2019-02-28 10:48:54 +1100

    [OSC-1238] Add compare load model to integration test

--- tests/integration/test_integration.sh
+++ tests/integration/test_integration.sh
@@ -1,11 +1,20 @@
 # Exit on failure
 set -e
 
-# Command alias for MCD
+# Bootstrap
+## Aliases
+loadModels='./tests/integration/models/load'
 mcd='pipenv run python mcd.py postgresql+psycopg2://postgres:travisci@localhost:5432/postgres'
+
+## Create stub load models
+mkdir $loadModels
+echo 'loadmodel1' > "$loadModels/load_model_1.json"
 
 # Begin new execution
 executionId=$($mcd init)
+
+# Compare load models
+$mcd compare $executionId load $loadModels **/*.json
 
 # Complete execution
 $mcd complete $executionId
