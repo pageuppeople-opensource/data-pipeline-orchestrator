66ad1f27866142e4af35b6cb80271944337149c4
Walter Ngo <ngo.walter@gmail.com>
2019-02-28 13:16:07 +1100

    [OSC-1238] Test failure scenario

--- tests/integration/test_integration.sh
+++ tests/integration/test_integration.sh
@@ -54,7 +54,7 @@ echo 'Beginning execution #2'
 InitExecution
 echo 'Modifying load_model_1'
 echo '' > "$loadModels/load_model_1.json"
-CompareAndAssert 'load_model_1'
+CompareAndAssert 'testfailure'
 CompleteAndAssert
 
 # debug
