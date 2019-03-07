7308695a71d6e8560cfa290107622535ac1a47cc
Chintan Raval <ChintanRaval@users.noreply.github.com>
2019-03-08 10:43:30 +1100

    [OSC-1247] - add coverage for all commands in integration tests, while also making then re-runnable

--- tests/integration/test_integration.sh
+++ tests/integration/test_integration.sh
@@ -4,27 +4,61 @@
 set -e
 
 # Bootstrap
-## Aliases
-modelDirectory="./tests/integration/models"
-loadModelDirectory="$modelDirectory/load"
-transformModelDirectory="$modelDirectory/transform"
-mcd="pipenv run python -m mcd postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+mcd="pipenv run python -m mcd postgresql+psycopg2://postgres:password@localhost:5432/postgres"
+
+# Generate a pseudo UUID
+NewUUID () {
+    local N B C='89ab'
+
+    for (( N=0; N < 16; ++N ))
+    do
+        B=$(( $RANDOM%256 ))
+
+        case $N in
+            6)
+                printf '4%x' $(( B%16 ))
+                ;;
+            8)
+                printf '%c%x' ${C:$RANDOM%${#C}:1} $(( B%16 ))
+                ;;
+            3 | 5 | 7 | 9)
+                printf '%02x-' $B
+                ;;
+            *)
+                printf '%02x' $B
+                ;;
+        esac
+    done
+
+    echo
+}
+
+LogErrorAndExit () {
+    local errorText=$1
+    local errorCode=1
+
+    >&2 echo ${errorText}
+    exit $errorCode
+}
+
+AssertAreEqual () {
+    local actual=$1
+    local expected=$2
+    if [ "$actual" != "$expected" ]
+    then
+        LogErrorAndExit "ERROR: expected "$expected", actual "$actual""
+    fi
+}
 
 InitExecution () {
     local executionId=$($mcd init-execution)
 
     if [ ${#executionId} -ne 36 ]
     then
-        echo "ERROR: expected a non-empty guid-length execution identifier, actual "$executionId""
-        exit 1
+        LogErrorAndExit "ERROR: expected a non-empty guid-length execution identifier, actual "$executionId""
     fi
 
-    # local  __resultvar=$1
-    # if [[ "$__resultvar" ]]; then
-    #     eval $__resultvar="'$executionId'"
-    # else
     echo "$executionId"
-    # fi
 }
 
 GetLastSuccessfulExecution () {
@@ -32,8 +66,7 @@ GetLastSuccessfulExecution () {
 
     if [ ${#executionId} != 36 ] && [ $executionId != 'NO_LAST_SUCCESSFUL_EXECUTION' ]
     then
-        echo "ERROR: expected a non-empty guid-length execution identifier or known constant when no matching execution found, actual "$executionId""
-        exit 1
+        LogErrorAndExit "ERROR: expected a non-empty guid-length execution identifier or known constant when no matching execution found, actual "$executionId""
     fi
 
     echo "$executionId"
@@ -45,8 +78,7 @@ GetExecutionLastUpdatedTimestamp () {
 
     if ! [[ $executionLastUpdatedTimestamp =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}[\+,-][0-9]{2}:[0-9]{2}$ ]]
     then
-        echo "ERROR: expected a non-empty ISO 8601 datetime with timezone, actual "$executionLastUpdatedTimestamp""
-        exit 1
+        LogErrorAndExit "ERROR: expected a non-empty ISO 8601 datetime with timezone, actual "$executionLastUpdatedTimestamp""
     fi
 
     echo "$executionLastUpdatedTimestamp"
@@ -56,6 +88,7 @@ PersistModels () {
     local executionId=$1
     local modelType=$2
     local basePath=$3
+
     $mcd persist-models $executionId $modelType $basePath "**/*.json" "**/*.csv" "**/*.sql"
 }
 
@@ -71,152 +104,111 @@ CompareModels () {
 
 CompleteExecution () {
     local executionId=$1
-    $mcd complete-execution $executionId
-}
 
-CompareAndAssert () {
-    echo "Comparing load models"
-    $($mcd persist-models $executionId load $loadModelDirectory "*.json")
-    lastSuccessfulExecutionId=$($mcd get-last-successful-execution)
-    changedModels=$($mcd compare-models $lastSuccessfulExecutionId $executionId load)
-
-    echo "Assert changed load models"
-    if [ "$changedModels" != "$1" ]
-    then
-        echo "ERROR: expected "$1", actual "$changedModels""
-        exit 1
-    fi
-}
-
-CompleteAndAssert () {
-    echo "Completing execution"
     $mcd complete-execution $executionId
 
-    echo "Asserting last successful execution"
-    lastSuccessfulExecutionId=$($mcd get-last-successful-execution)
-    if [ $lastSuccessfulExecutionId != $executionId ]
-    then
-        echo "ERROR: expected \"$executionId\", actual '$lastSuccessfulExecutionId'"
-        exit 1
-    fi
+    lastSuccessfulExecId=$(GetLastSuccessfulExecution)
+    AssertAreEqual "$lastSuccessfulExecId" "$executionId"
 }
 
-## Setup
+modelDirectory="./tests/integration/models"
+loadModelDirectory="$modelDirectory/load"
+transformModelDirectory="$modelDirectory/transform"
+load_model_1="load_model_1_$(NewUUID)"
+load_model_2="load_model_2_$(NewUUID)"
+transform_model_1="transform_model_1_$(NewUUID)"
+transform_model_2="transform_model_2_$(NewUUID)"
+transform_model_3="transform_model_3_$(NewUUID)"
+transform_model_4="transform_model_4_$(NewUUID)"
+
+###############
+# Execution 1 #
+###############
+echo -e "\nBeginning execution #1"
+
+# ARRANGE
+## Remove test models' directory, if exists
 rm -rf $modelDirectory
 
 ## Create stub load models
-echo "Creating stub LOAD models: load_model_1.json, load_model_2.json"
+echo "Creating stub LOAD models: load_model_1, load_model_2"
 mkdir -p $loadModelDirectory
-echo "load_model_1" > "$loadModelDirectory/load_model_1.json"
-echo "load_model_2" > "$loadModelDirectory/load_model_2.json"
+echo "load_model_1" > "$loadModelDirectory/$load_model_1.json"
+echo "load_model_2" > "$loadModelDirectory/$load_model_2.json"
 
 ## Create stub transform models
-echo "Creating stub TRANSFORM models: transform_model_1.csv, transform_model_2.sql, transform_model_3.sql"
+echo "Creating stub TRANSFORM models: transform_model_1, transform_model_2, transform_model_3"
 mkdir -p $transformModelDirectory
-echo "transform_model_1" > "$transformModelDirectory/transform_model_1.csv"
-echo "transform_model_2" > "$transformModelDirectory/transform_model_2.sql"
-echo "transform_model_3" > "$transformModelDirectory/transform_model_3.sql"
+echo "transform_model_1" > "$transformModelDirectory/$transform_model_1.csv"
+echo "transform_model_2" > "$transformModelDirectory/$transform_model_2.sql"
+echo "transform_model_3" > "$transformModelDirectory/$transform_model_3.sql"
 
-# Execution 1
-echo "Beginning execution #1"
-
-iter1_ExecId=$(InitExecution)
-echo "iter1_ExecId = $iter1_ExecId"
+# ACT & ASSERT
+iter1_execId=$(InitExecution)
+echo " iter1_execId                                = $iter1_execId"
 
 iter1_lastSuccessfulExecId=$(GetLastSuccessfulExecution)
-echo "iter1_lastSuccessfulExecId = $iter1_lastSuccessfulExecId"
+echo " iter1_lastSuccessfulExecId                  = $iter1_lastSuccessfulExecId"
 
 iter1_lastSuccessfulExecCompletionTimestamp=$(GetExecutionLastUpdatedTimestamp $iter1_lastSuccessfulExecId)
-echo "iter1_lastSuccessfulExecCompletionTimestamp = $iter1_lastSuccessfulExecCompletionTimestamp"
-# GetExecutionLastUpdatedTimestamp $iter1_lastSuccessfulExecId
+echo " iter1_lastSuccessfulExecCompletionTimestamp = $iter1_lastSuccessfulExecCompletionTimestamp"
 
-PersistModels $iter1_ExecId LOAD $loadModelDirectory
-iter1_comparedLoadModels=$(CompareModels $iter1_lastSuccessfulExecId $iter1_ExecId LOAD)
-echo "iter1_comparedLoadModels = '$iter1_comparedLoadModels'"
+PersistModels $iter1_execId LOAD $loadModelDirectory
+iter1_changedLoadModels=$(CompareModels $iter1_lastSuccessfulExecId $iter1_execId LOAD)
+echo " iter1_changedLoadModels                     = '$iter1_changedLoadModels'"
 
-# Assert
-iter1_compareLoadModels_expected="load_model_1 load_model_2"
-if [ "$iter1_comparedLoadModels" != "$iter1_compareLoadModels_expected" ]
-then
-    echo "ERROR: expected '"$iter1_compareLoadModels_expected"', actual '"$iter1_comparedLoadModels"'"
-    exit 1
-fi
+iter1_changedLoadModels_expected="$load_model_1 $load_model_2"
+AssertAreEqual "$iter1_changedLoadModels" "$iter1_changedLoadModels_expected"
 
-PersistModels $iter1_ExecId TRANSFORM $transformModelDirectory
-iter1_comparedTransformModels=$(CompareModels $iter1_lastSuccessfulExecId $iter1_ExecId TRANSFORM)
-echo "iter1_comparedTransformModels = '$iter1_comparedTransformModels'"
+PersistModels $iter1_execId TRANSFORM $transformModelDirectory
+iter1_changedTransformModels=$(CompareModels $iter1_lastSuccessfulExecId $iter1_execId TRANSFORM)
+echo " iter1_changedTransformModels                = '$iter1_changedTransformModels'"
 
-# Assert
-iter1_compareTransformModels_expected="transform_model_1 transform_model_2 transform_model_3"
-if [ "$iter1_comparedTransformModels" != "$iter1_compareTransformModels_expected" ]
-then
-    echo "ERROR: expected '"$iter1_compareTransformModels_expected"', actual '"$iter1_comparedTransformModels"'"
-    exit 1
-fi
+iter1_changedTransformModels_expected="$transform_model_1 $transform_model_2 $transform_model_3"
+AssertAreEqual "$iter1_changedTransformModels" "$iter1_changedTransformModels_expected"
 
-CompleteExecution $iter1_ExecId
+CompleteExecution $iter1_execId
 
-# Execution 2
-echo "Beginning execution #2"
+###############
+# Execution 2 #
+###############
+echo -e "\nBeginning execution #2"
 
-iter2_ExecId=$(InitExecution)
-echo "iter2_ExecId = $iter2_ExecId"
-
-iter2_lastSuccessfulExecId=$(GetLastSuccessfulExecution)
-echo "iter2_lastSuccessfulExecId = $iter2_lastSuccessfulExecId"
-if [ "$iter2_lastSuccessfulExecId" != "$iter1_ExecId" ]
-then
-    echo "ERROR: expected '"$iter1_ExecId"', actual '"$iter2_lastSuccessfulExecId"'"
-    exit 1
-fi
-
-iter2_lastSuccessfulExecCompletionTimestamp=$(GetExecutionLastUpdatedTimestamp $iter1_ExecId)
-echo "iter2_lastSuccessfulExecCompletionTimestamp = $iter2_lastSuccessfulExecCompletionTimestamp"
-
-PersistModels $iter2_ExecId LOAD $loadModelDirectory
-iter2_comparedLoadModels=$(CompareModels $iter1_ExecId $iter2_ExecId LOAD)
-echo "iter2_comparedLoadModels = '$iter2_comparedLoadModels'"
-
-# Assert
-iter2_compareLoadModels_expected=""
-if [ "$iter2_comparedLoadModels" != "$iter2_compareLoadModels_expected" ]
-then
-    echo "ERROR: expected '"$iter2_compareLoadModels_expected"', actual '"$iter2_comparedLoadModels"'"
-    exit 1
-fi
-
+# ARRANGE
+echo "Making no changes to any LOAD models"
 
 echo "Modifying transform_model_2"
-echo "Modified transform_model_2" > "$transformModelDirectory/transform_model_2.sql"
+echo "Modified transform_model_2" > "$transformModelDirectory/$transform_model_2.sql"
+
 echo "Deleting transform_model_3"
-rm -f "$transformModelDirectory/transform_model_3.sql"
-echo "Adding transform_model_2"
-echo "transform_model_4" > "$transformModelDirectory/transform_model_4.sql"
+rm -f "$transformModelDirectory/$transform_model_3.sql"
 
-PersistModels $iter2_ExecId TRANSFORM $transformModelDirectory
-iter2_comparedTransformModels=$(CompareModels $iter1_ExecId $iter2_ExecId TRANSFORM)
-echo "iter2_comparedTransformModels = '$iter2_comparedTransformModels'"
+echo "Adding transform_model_4"
+echo "transform_model_4" > "$transformModelDirectory/$transform_model_4.sql"
 
-# Assert
-iter2_compareTransformModels_expected="transform_model_2 transform_model_4"
-if [ "$iter2_comparedTransformModels" != "$iter2_compareTransformModels_expected" ]
-then
-    echo "ERROR: expected '"$iter2_compareTransformModels_expected"', actual '"$iter2_comparedTransformModels"'"
-    exit 1
-fi
+# ACT & ASSERT
+iter2_execId=$(InitExecution)
+echo " iter2_execId                                = $iter2_execId"
 
-CompleteExecution $iter2_ExecId
+iter2_lastSuccessfulExecId=$(GetLastSuccessfulExecution)
+echo " iter2_lastSuccessfulExecId                  = $iter2_lastSuccessfulExecId"
+AssertAreEqual "$iter2_lastSuccessfulExecId" "$iter1_execId"
 
+iter2_lastSuccessfulExecCompletionTimestamp=$(GetExecutionLastUpdatedTimestamp $iter2_lastSuccessfulExecId)
+echo " iter2_lastSuccessfulExecCompletionTimestamp = $iter2_lastSuccessfulExecCompletionTimestamp"
 
-# CompareAndAssert "load_model_1 load_model_2"
-# CompleteAndAssert
+PersistModels $iter2_execId LOAD $loadModelDirectory
+iter2_changedLoadModels=$(CompareModels $iter2_lastSuccessfulExecId $iter2_execId LOAD)
+echo " iter2_changedLoadModels                     = '$iter2_changedLoadModels'"
 
-# Modify load_model_1
-# echo "Modifying load_model_1"
-# echo "" > "$loadModelDirectory/load_model_1.json"
+iter2_compareLoadModels_expected=""
+AssertAreEqual "$iter2_changedLoadModels" "$iter2_compareLoadModels_expected"
 
-# Execution 2
-# echo "Beginning execution #2"
-# InitExecution
-# CompareAndAssert "load_model_1"
-# CompleteAndAssert
+PersistModels $iter2_execId TRANSFORM $transformModelDirectory
+iter2_changedTransformModels=$(CompareModels $iter2_lastSuccessfulExecId $iter2_execId TRANSFORM)
+echo " iter2_changedTransformModels                = '$iter2_changedTransformModels'"
+
+iter2_compareTransformModels_expected="$transform_model_2 $transform_model_4"
+AssertAreEqual "$iter2_changedTransformModels" "$iter2_compareTransformModels_expected"
+
+CompleteExecution $iter2_execId
