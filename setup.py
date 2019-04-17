1b9e486a6af7480fea48f54281cf86f4d45a5c34
Sean Budd <seanbudd123@gmail.com>
2019-04-17 14:17:53 +1000

    bump dependencies (#23)

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(name='dpo',
       version='0.1.1',
       packages=find_packages(),
       install_requires=[
-          'psycopg2-binary==2.7.7',
-          'SQLAlchemy==1.2.17',
-          'alembic==1.0.8',
+          'psycopg2-binary==2.8.2',
+          'SQLAlchemy==1.3.3',
+          'alembic==1.0.9',
       ],
       package_data={
           '': ['alembic.ini', 'alembic/*.py', 'alembic/**/*.py'],
