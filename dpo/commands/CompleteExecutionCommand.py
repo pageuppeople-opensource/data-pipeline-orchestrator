8335d5fcae72b199d7970c3b785e362a105b9dec
Chintan Raval <ChintanRaval@users.noreply.github.com>
2019-05-15 11:24:45 +1000

    log stats upon completion

--- dpo/commands/CompleteExecutionCommand.py
+++ dpo/commands/CompleteExecutionCommand.py
@@ -9,3 +9,12 @@ class CompleteExecutionCommand(BaseCommand):
     def execute(self):
         execution = self.repository.complete_execution(self._execution_id)
         self.logger.debug('Completed Execution: ' + str(execution))
+
+        total_execution_seconds = execution.execution_time_ms // 1000
+        execution_hours = total_execution_seconds // 3600
+        execution_minutes = (total_execution_seconds // 60) % 60
+        execution_seconds = total_execution_seconds % 60
+        self.logger.info(f'Completed Execution ID: {execution.execution_id}; '
+                         f'Started On: {execution.started_on.isoformat()}; '
+                         f'Completed On: {execution.completed_on.isoformat()}; '
+                         f'Execution Time: {execution_hours}h {execution_minutes}m {execution_seconds}s.')
